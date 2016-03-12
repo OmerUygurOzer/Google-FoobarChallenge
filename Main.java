919030ded9794dc858e7ca3706c04f0ae61951ec
OmerUygurOzer <omer.u.ozer@gmail.com>
2016-03-11 17:23:29 -0800

    init

---
+++ Main.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Omer on 3/11/2016.
+ */
+
+
+
+/*
+==================
+This is the main class that will call contain and call all the answer() methods and classes
+ */
+
+
+public class Main {
+    public static void main (String[] arg) {
+        System.out.println("test");
+    }
+}
